9b1936967cc2d774f8572a229c52e384c2d30185
calling0108 <74917625+calling0108@users.noreply.github.com>
2023-11-20 15:41:30 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -2,12 +2,13 @@ print("Hello python")
 
 def rocket():
     a = 0
-    b = True
+    b = False
     while not b:
         a += 1
         if a == 10:
-            b = False
             print("'a' is Ten!")
+            b = True
+            
 
 rocket()
 
