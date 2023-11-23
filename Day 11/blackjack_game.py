c1a6f505da6e8dae7205c8dc2a25046b7d0009f6
calling0108 <74917625+calling0108@users.noreply.github.com>
2023-11-23 21:47:20 +0900

    enter

--- Day 11/blackjack_game.py
+++ Day 11/blackjack_game.py
@@ -7,7 +7,7 @@ def draw_card():
     p1_card = random.choice(card_list)
 
     print(f"Your cards: [{u1_card}, {u2_card} ]current score: {u1_card + u2_card}")
-    print(f"Computer's first card: {p_1_card}")
+    print(f"Computer's first card: {p1_card}")
 
 start_answer = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
 if start_answer == "y":
@@ -26,3 +26,4 @@ while not a: # user 카드 뽑기 과정 반복
     keep_game = input("Type 'y' to get another card, type 'n' to pass: ")
 
 if keep_game == "n":
+    print("yes")
