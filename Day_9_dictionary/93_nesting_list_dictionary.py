68e17ac9e5aea43ae2490a0ec51216bcdc8cf9b1
calling0108 <74917625+calling0108@users.noreply.github.com>
2024-01-08 23:56:53 +0900

    enter

--- Day_9_dictionary/93_nesting_list_dictionary.py
+++ Day_9_dictionary/93_nesting_list_dictionary.py
@@ -14,15 +14,20 @@ travel_log = {
 #Nesting Dictionary in a Dictionary
 
 travel_log = {
-  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+  "France": {
+      "cities_visited": ["Paris", "Lille", "Dijon"], 
+      "total_visits": 12
+      },
+  "Germany": {
+      "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+      "total_visits": 5},
 }
 
 print(travel_log["France"]["cities_visited"])
 
 #Nesting Dictionaries in Lists
 
-travel_log = [
+travel_log_2 = [
 {
   "country": "France", 
   "cities_visited": ["Paris", "Lille", "Dijon"], 
@@ -34,3 +39,4 @@ travel_log = [
   "total_visits": 5,
 },
 ]
+
