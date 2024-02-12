d2e40450e711b7fc357989906656ee337cc44760
calling0108 <74917625+calling0108@users.noreply.github.com>
2024-02-12 22:54:39 +0900

    enter

--- Day16_OOP/venv/main.py
+++ Day16_OOP/venv/main.py
@@ -1,1 +1,19 @@
-from prettytable import
+from prettytable import PrettyTable
+
+table = PrettyTable()
+# print(table)
+
+table.field_names = ["Pokemon Name", "Type"]
+
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["charmander", "Fire"]
+    ]
+)
+
+# table.align = "c"
+table.get_string(align = "l")
+
+print(table)
