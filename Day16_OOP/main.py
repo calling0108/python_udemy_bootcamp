a207438bb165c7a5a1d32b9250d4933d444a1db2
calling0108 <74917625+calling0108@users.noreply.github.com>
2024-02-12 20:52:49 +0900

    enter

---
+++ Day16_OOP/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle() # timmy는 object, Turtle()은 class
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+
